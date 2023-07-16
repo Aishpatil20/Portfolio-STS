68eb081263e2003d104a977ac32898ed44e7d72e
Aishpatil20 <aishwaryadpatil02@gmail.com>
2023-07-16 12:55:21 +0530

    initial

---
+++ src/main/java/com/sts/webapp/PortfolioStsApplication.java
@@ -0,0 +1,15 @@
+package com.sts.webapp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class PortfolioStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioStsApplication.class, args);
+	}
+
+}
